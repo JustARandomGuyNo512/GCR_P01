f8cfaf0275970edecd859961af7b617ba3c40f65
Sheridan <10986176+sheridan1024@user.noreply.gitee.com>
2026-07-20 11:05:53 +0800

    ``` chore(rendering): 增加纹理单元最大限制并添加注释
    - 将纹理单元最大数量从12增加到128
    - 添加关于Iris实现方式的注释说明
    ```

--- src/main/java/com/sheridan/gcr/mixin/MixinGlStateManagerTextures.java
+++ src/main/java/com/sheridan/gcr/mixin/MixinGlStateManagerTextures.java
@@ -7,7 +7,9 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin({GlStateManager.class})
 public class MixinGlStateManagerTextures {
-
+    /*
+    * 我不到啊，我看Iris这么干的我也这么干了:D
+    * */
     @ModifyConstant(method = "<clinit>", constant = @Constant(intValue = 12), require = 0)
     private static int iris$increaseMaximumAllowedTextureUnits(int existingValue) {
         return 128;
